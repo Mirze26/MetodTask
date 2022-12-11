6b395afc134c3fea47936f4751d0b4ea8acd47e1
unknown <mirzaab@code.edu.az>
2022-12-11 17:14:38 +0400

    TaskTen

---
+++ TaskOne/Program.cs
@@ -0,0 +1,225 @@
+﻿
+
+
+//n - ədədinin 3 - ə və 7 - ə bölünüb - bölünməməsini tapın
+
+//ShowMod();
+//static void ShowMod()
+
+//{ 
+//    int n = 21;
+//	if (n % 3 == 0 && n % 7 == 0) ;
+
+//	{
+//		Console.WriteLine("Bolunur");
+//	}
+
+//}
+
+// n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın
+
+
+//Sum();
+//static void Sum()
+//{
+//      int n = 2;
+//      int m = 8;
+//      int sum = 0;
+//    if (n % 2 == 0 && m % 2 == 0) ;
+//    {
+//        sum = n + m;
+
+//        Console.WriteLine(sum);
+//    }
+
+//}
+
+// n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+//ResuLt();
+//static void ResuLt()
+//{
+//     int n = 1;
+//    int m = 20;
+//    int result = 0;
+//    for (int i = n; i <=m; i++)
+//    {
+//        if (i % 2 == 1)
+
+//        {
+//            result = result + i;
+
+
+//        }
+//    }
+//    Console.WriteLine(result);
+
+//}
+
+
+
+//arrayin icindeki tek ededlerin cemini tapin.
+
+
+//NumBers();
+//static void NumBers()
+//{
+
+//    int[] nums = { 1, 3, 5, 7, 11, 13,};
+//    int sum = 0;
+//    for (int i = 0; i < nums.Length; i++)
+//    {
+//    	if (nums[i] % 2 == 1) ;
+//    	{
+//    		sum += nums[i];
+//    	}
+//    }
+//    Console.WriteLine(sum);
+
+
+//}
+
+
+
+
+//arrayin icindeki cut ededlerin sayini tapin
+
+
+//ArrCount();
+//static void ArrCount()
+//{
+
+//    int[] nums = { 2, 4, 6, 8, 10, 12,14 };
+//    int count = 0;
+//    for (int i = 0; i < nums.Length; i++)
+//    {
+//        if (nums[i] % 2 == 0)
+//        {
+//            count+=nums[i];
+
+//        }
+
+//    }
+//    Console.WriteLine(count);
+
+
+//}
+//n ve m (n<m) ededleri arasindaki tek ededlerin sayini tapin.
+
+//ShowCount();
+//static void ShowCount()
+//{
+
+
+//    int n = 1;
+//    int m = 10;
+//    int count = 0;
+
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            count = i++;
+
+//        }
+
+//    }
+//    Console.WriteLine(count);
+
+//}
+
+
+// n ve m (n<m) ededleri arasindaki cut ededlerin cemini tapin.
+
+//EdedinCemi();
+//static void EdedinCemi()
+//{
+//    int n = 3;
+//    int m = 30;
+//    int sum = 0;
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i%2==0)
+//        {
+//            sum = i++;
+
+//        }
+
+//    }
+//    Console.WriteLine(sum);
+
+//}
+
+//n ve m (n<m) ededleri arasindaki cut ededlerin sayini tapin.
+
+
+//EdedinSayi();
+//static void EdedinSayi()
+//{
+//    int n = 12;
+//    int m = 22;
+//    int count = 0;
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 2 == 0)
+//        {
+//            count++;
+
+//        }
+//    }
+//    Console.WriteLine(count);
+//}
+
+
+
+//n tam ededinin sade ve ya murekkeb oldugunu tapin.
+
+//SadeMurekkeb();
+//static void SadeMurekkeb()
+//{
+//    int n = 24;
+//    int count = 0;
+//    if (n<=0)
+//    {
+//        Console.WriteLine("duzgun eded daxil eliyun");
+//    }
+//    if (n==1)
+//    {
+//        Console.WriteLine("sade ve murekkeb deyil");
+//    }
+//    for (int i = 1; i <=n; i++)
+//    {
+//        if (n%i==0)
+//        {
+//            count++;
+//        }
+//    }
+
+//    if (count >2)
+//    {
+//        Console.WriteLine("murekkeb");
+//    }
+//    else
+//    {
+//        Console.WriteLine("sade");
+//    }
+
+//n ededinin faktorialini hesablamaq
+
+//EdedinHasili();
+//static void EdedinHasili()
+//{
+
+
+
+//    int n = 6;
+//    int m = 6;
+//    int sum = 0;
+//    for (int i = n; i <= m; i++)
+//    {
+//        sum = n * m;
+
+
+//        Console.WriteLine(sum);
+//    }
+//}
